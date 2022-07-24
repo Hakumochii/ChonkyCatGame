46250bf70ba7b717061b73ca71b834f3a2121ab7
UNF-GDC <109671887+UNF-GDC@users.noreply.github.com>
2022-07-24 20:50:58 +0200

    Træer kan despawne og fortælle andre træer at spawne. Teknisk set endless runner nu

--- GDC-project/Assets/Scripts/Endless/TreeDeathSpawn.cs
+++ GDC-project/Assets/Scripts/Endless/TreeDeathSpawn.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class TreeDeathSpawn : MonoBehaviour
 {
+    [SerializeField] private TreeDeathSpawn spawnedTreesScript;
+
     [SerializeField] private GameObject[] treePrefabs;
 
-    [SerializeField] private float spawnDistance = 50f;
+    [SerializeField] private float spawnDistance = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +19,33 @@ public class TreeDeathSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Once behind the camera, destroy self
         if (Camera.main.transform.position.z > transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Tell the next tree to spawn a tree<s
+        spawnedTreesScript.GoSpawn();
+    }
+
+    public void GoSpawn()
+    {
+        // If this has spawned a tree, tell that tree to spawn one
+        if (spawnedTreesScript != null)
+        {
+            spawnedTreesScript.GoSpawn();
+        } // If haven't spawned a tree, spawn one
+        else
         {
             // Gets location X meters ahead
             Vector3 newSpawnPos = transform.position + new Vector3(0f, 0f, spawnDistance);
 
-            GameObject newTree = Instantiate(treePrefabs[int(Random.Range(0f, treePrefabs.Length))], transform.position)
-            Destroy(gameObject);
+            // Spawns new tree X meters ahead and remembers its script
+            spawnedTreesScript = Instantiate(treePrefabs[Mathf.FloorToInt(Random.Range(0f, treePrefabs.Length))], newSpawnPos, Quaternion.identity).GetComponent<TreeDeathSpawn>();
         }
     }
 }
