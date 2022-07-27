649086170116b8ce99993462fbc33c8551d3cde0
UNF-GDC <109671887+UNF-GDC@users.noreply.github.com>
2022-07-27 15:26:32 +0200

    :earth_africa: No empty space from ground

---
+++ GDC-project/Assets/Scripts/Fishies/FishSpawner.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject fishyPrefab;
+    private GameObject myFishy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myFishy = Instantiate(fishyPrefab, transform);
+
+        float xCoord = Random.Range(-0.5f, 0.5f);
+
+        float zCoord = FindZByX(xCoord);
+
+        myFishy.transform.localPosition = new Vector3(xCoord, 0.5f, zCoord);
+    }
+
+    private float FindZByX(float xCoord)
+    {
+        float endFloat = 0;
+
+        // Do linear equation to find the max X
+        if (xCoord >= 0)
+        {
+            endFloat = -xCoord * 0.5f + 0.5f;
+        }
+        else
+        {
+            endFloat = xCoord * 0.5f + 0.5f;
+        }
+
+        // Choose a random point within the range
+        endFloat = Random.Range(-endFloat, endFloat);
+
+        return endFloat;
+    }
+
+    // Destroys enemy before death to avoid the duplication thingy thing
+    private void OnDestroy()
+    {
+        Destroy(myFishy);
+    }
+}
